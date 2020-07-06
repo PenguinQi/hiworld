a28c105ad0f90184b628929b63f641fa8e5b487a
PenguinQi <qiean24@outlook.com>
2020-07-06 14:27:09 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @Author Administrator
+ * @Version 1.0
+ * @Date 2020-07-06 14:20
+ * @Since jdk 1.8.0_191
+ * 类说明：
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
